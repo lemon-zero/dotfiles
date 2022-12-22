6c0231e542fa7fe089b9f83959a8f98458775f51
Diego Ezequiel <diego.scardulla@outlook.com>
2022-12-21 21:48:25 -0300

    added nvim-cmp extra keybinds

--- .config/nvim/lua/zxc/lsp/lsp.lua
+++ .config/nvim/lua/zxc/lsp/lsp.lua
@@ -53,3 +53,25 @@ vim.diagnostic.config({
 	severity_sort = false,
 	float = true,
 })
+
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+	return
+end
+
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+local cmp_config = lsp.defaults.cmp_config({
+	window = {
+		completion = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+})
+
+cmp.setup(cmp_config)
