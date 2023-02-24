a29fccecf156595f2864bbf7a9e2af6d3efcbcd7
Diego Ezequiel <diego.ezequiel.scardulla@gmail.com>
2023-02-24 00:34:18 -0300

    cleanup lazy-nvim.lua

--- .config/nvim/lua/lazy-nvim.lua
+++ .config/nvim/lua/lazy-nvim.lua
@@ -13,44 +13,4 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-local plugins = {
-	{ "nvim-telescope/telescope.nvim", version = "*", dependencies = { "nvim-lua/plenary.nvim" } },
-	{
-		-- Highlight, edit, and navigate code
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		config = function()
-			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-		end,
-	},
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{
-		-- Comments
-		"numToStr/Comment.nvim",
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-	},
-	-- Colorschemes
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-	},
-	"nyoom-engineering/oxocarbon.nvim",
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-	},
-	"ellisonleao/gruvbox.nvim",
-	"lunarvim/darkplus.nvim",
-	-- Transparency
-	"xiyaowong/nvim-transparent",
-	-- Status Bar
-	"nvim-lualine/lualine.nvim",
-	-- Css Colors
-	"NvChad/nvim-colorizer.lua",
-}
 require("lazy").setup("plugins")
